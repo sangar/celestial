873eee2c18fafecb3a34f6427b904a9037245031
Gard Sandholt <gard.sandholt@gmail.com>
2020-01-23 18:50:00 +0100

    Cleaned up test

--- test/celestial_test.rb
+++ test/celestial_test.rb
@@ -6,21 +6,8 @@ class CelestialTest < Minitest::Test
   end
 
   def test_can_get_data_by_search
-    datas = []
-    31.times do |i|
-      data = Celestial.for(
-        latitude: 59.9128627,
-        longitude: 10.7434443,
-        date: DateTime.new(2020, 1, (1 + i), 0, 0, 0, '+01:00')
-      )
-      datas << data
-    end
+    data = Celestial.for(latitude: 59.9128627, longitude: 10.7434443)
 
-    puts "datas:"
-    datas.each do |data|
-      puts "#{data[:sunrise].day}, sunrise: #{data[:sunrise].strftime("%T")}, sunset: #{data[:sunset].strftime("%T")}, length: #{data[:length]}, difference: #{data[:difference]}"
-    end
-
-    assert_equal 9, datas.first.count
+    assert_equal 9, data.count
   end
 end
